65887613037b3b9b0c5eda949b37ff1d8cbe8bc9
kateann19 <mcgeehee@gmail.com>
2020-08-21 13:55:37 -0400

    robot sort finally working

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -57,9 +57,7 @@ class SortingRobot:
         """
         self._time += 1
         # Swap the held item with the list item at the robot's position
-        print(f"{self._item},{self._list[self._position]}")
         self._item, self._list[self._position] = self._list[self._position], self._item
-        print(f"{self._item} {self._list[self._position]}")
     ###########################################
     
     def compare_item(self):
@@ -70,7 +68,6 @@ class SortingRobot:
         If the held item's value is equal, return 0.
         If either item is None, return None.
         """
-        print(f"Inside compare {self._item} {self._position}")
         if self._item is None or self._list[self._position] is None:
             return None
         elif self._item > self._list[self._position]:
@@ -127,15 +124,10 @@ class SortingRobot:
                 self.move_right()
                
                 if self.compare_item() == 1:
-                    #self.move_left()
                     self.swap_item()
-                    self.set_list(self.get_position())
-                    #self.move_right()
-                    self.set_light_on()
-                    print(self.get_list())
-                
-            self.reset_position()
-                
+                    self.set_list(i)
+                    self.set_light_on() 
+            self.reset_position()   
             if self.light_is_on() == False:
                 return self.get_list()
 
@@ -144,8 +136,8 @@ if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 90, 54, 5, 0, 87, 9]
-    #l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    #l = [15, 41, 58, 49, 26, 90, 54, 5, 0, 87, 9]
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
 
